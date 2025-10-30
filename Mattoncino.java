0ccf7d8c114989806c30c86802a3f9af1a838d79
BELLAFKIH.22128 <bellafkih.deyae.studente@itispaleocapa.it>
2025-10-30 09:00:50 +0100

    Lego

---
+++ Mattoncino.java
@@ -0,0 +1,53 @@
+public class Mattoncino {
+    private int codice;
+    private int red;
+    private int green;
+    private int blue;
+    private int peso;
+    private int larghezza;
+    private int lunghezza;
+    private int altezza;
+
+    public Mattoncino (int codice, int red, int green, int blue, int peso, int larghezza, int lunghezza, int altezza) {
+        this.codice = codice;
+        this.red = red;
+        this.green = green;
+        this.blue = blue;
+        this.peso = peso;
+        this.larghezza = larghezza;
+        this.lunghezza = lunghezza;
+        this.altezza = altezza;
+    }
+
+    public int getCodice() {
+        return codice;
+    }
+
+    public int getRed() {
+        return red;
+    }
+
+    public int getGreen() {
+        return green;
+    }
+
+    public int getBlue(){
+        return blue;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+
+    public int getLarghezza() {
+      return larghezza;
+    }
+
+    public int getLunghezza() {
+        return lunghezza;
+    }
+
+    public int getAltezza(){
+        return altezza;
+    }
+}
---
+++ Prodotto.java
@@ -0,0 +1,18 @@
+public class Prodotto {
+    private int codice;
+    private String denominazione;
+    private Mattoncino[] mattoncini;
+
+    public Prodotto (int codice, String denominazione) {
+        this.codice = codice;
+        this. denominazione = denominazione;
+        this.mattoncini = new Mattoncino[10];
+    }
+    public void aggiungiMattoncino() {
+        for (int i = 0; i < mattoncini.length; i++) {
+
+        }
+    }
+
+
+}
